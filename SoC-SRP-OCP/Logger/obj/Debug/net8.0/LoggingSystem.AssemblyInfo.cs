44ab18375334e7f8886e05f731b77220d6c1e6b2
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-03-05 16:48:21 +0200

    committing directory restructuring

--- SoC-SRP-OCP/Logger/obj/Debug/net8.0/LoggingSystem.AssemblyInfo.cs
+++ SoC-SRP-OCP/Logger/obj/Debug/net8.0/LoggingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4f43b092bb949f663d60f1a3a31877f49cb17d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b317b510f9a4da699fd0ade85fa5766933e3dbb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
